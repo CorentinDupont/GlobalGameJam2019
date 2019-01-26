a05bb4f7f5c3d4700d23cb7e262e7a563fb88318
CorentinDupont <dupont.corentin44@gmail.com>
2019-01-26 18:18:51 +0100

    Draggable object and player grab and drop

---
+++ Assets/Core/Player/Drag.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+
+    public int dragDistance;
+    private Camera camera;
+    private Draggable targetedDraggableObject;
+    private Draggable draggingObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Raycast to hit 
+        RaycastHit hitInfo;
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hitInfo, dragDistance)){
+
+            //if hit object is type draggable
+            if (hitInfo.transform.gameObject.GetComponent<Draggable>()) {
+                Draggable hitDraggableObject = hitInfo.transform.gameObject.GetComponent<Draggable>();
+                //if this object can be draggable 
+                if(hitDraggableObject.isDraggable){
+                    hitDraggableObject.targetObject();
+                    targetedDraggableObject = hitDraggableObject;
+                }                
+            }
+
+        }else if(targetedDraggableObject != null){
+            targetedDraggableObject.unTargetObject();
+            targetedDraggableObject = null;
+        }
+
+        //Click to make an action on a draggable object
+        if (Input.GetMouseButtonDown(0)){
+            if(targetedDraggableObject != null && draggingObject == null){
+                //grab the object
+                targetedDraggableObject.gameObject.transform.parent = this.transform;
+                targetedDraggableObject.gameObject.transform.localPosition = GameObject.Find("DraggableObjectSocket").transform.localPosition;
+                targetedDraggableObject.isDraggable = false;
+                targetedDraggableObject.GetComponent<Rigidbody>().isKinematic = true;
+                targetedDraggableObject.unTargetObject();
+                draggingObject = targetedDraggableObject;
+                targetedDraggableObject = null;
+            }else if(targetedDraggableObject == null && draggingObject != null){
+                //leave the object
+                draggingObject.gameObject.transform.parent = draggingObject.initialParent;
+                draggingObject.isDraggable = true;
+                draggingObject.GetComponent<Rigidbody>().isKinematic = false;
+                draggingObject = null;
+
+            }
+        }
+    }
+
+    // void GrabObject(Draggable object){
+        
+    // }
+}
